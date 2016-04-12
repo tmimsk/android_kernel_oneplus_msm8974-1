379a685608d52c03b6159474c1d328953441c204
Sk Kim <sookwone@gmail.com>
2016-04-12 00:01:42 -0400

    enable by default

--- arch/arm/kernel/autosmp.c
+++ arch/arm/kernel/autosmp.c
@@ -61,8 +61,8 @@ static struct asmp_param_struct {
 };
 
 static unsigned int cycle = 0;
-static int enabled __read_mostly = 0;
-static int enable_switch = 0;
+static int enabled __read_mostly = 1;
+static int enable_switch = 1;
 
 static void reschedule_hotplug_work(void)
 {
